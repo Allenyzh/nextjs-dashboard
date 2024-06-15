0d1ff8475a80b568d46ecb584c4028ac1d325559
Allenyzh <allenyangzhenhao@gmail.com>
2024-06-14 23:44:32 -0400

    1

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
 
-
 import {
   CustomerField,
   CustomersTableType,
@@ -171,6 +170,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice); // Invoice is an empty array []
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
